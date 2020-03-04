a01a95d04219acc81be30f4a4271d5006b4033e2
nickallama <me@nicholasneale.com>
2020-03-04 23:45:37 +0000

    Custom babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,30 @@
+const envConfig = {
+    useBuiltIns: 'entry',
+    corejs: 3
+};
+
+const serverEnvConfig = {
+    useBuiltIns: 'entry',
+    corejs: 3,
+    targets: {
+        node: true
+    }
+};
+
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    presets: [['@babel/preset-env', envConfig], '@vue/babel-preset-app'],
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-optional-chaining'
+    ],
+    env: {
+        test: {
+            plugins: [
+                ['babel-plugin-webpack-alias']
+            ]
+        },
+        node: {
+            presets: [['@babel/preset-env', serverEnvConfig]]
+        }
+    }
+};
