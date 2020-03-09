8eaf1ff77f3286171801462206767efb0dd4c37d
nickallama <me@nicholasneale.com>
2020-03-09 21:13:21 +0000

    Handle sass preloader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,34 @@
+const path = require('path');
+
 module.exports = {
     stories: ['../src/**/*.stories.(js|mdx)'],
-    addons: ['@storybook/addon-docs']
+    addons: ['@storybook/addon-docs'],
+    webpackFinal: config => ({
+        ...config,
+        resolve: {
+            ...config.resolve,
+            alias: {
+                ...config.resolve.alias,
+                '@': path.join(__dirname, '../src/')
+            }
+        },
+        module: {
+            rules: [
+                ...config.module.rules,
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'vue-style-loader',
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                prependData: `@import "@/theme/_defaultTheme.scss";`
+                            }
+                        }
+                    ],
+                }
+            ]
+        }
+    })
 };
