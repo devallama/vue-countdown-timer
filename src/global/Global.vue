e741d16ee37ec6685d7732b15b47d4312fc1710b
nickallama <me@nicholasneale.com>
2020-04-14 21:33:06 +0100

    Global styling

---
+++ src/global/Global.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Global'
+};
+</script>
+
+<style lang="scss">
+    body {
+        padding: 0;
+        margin: 0;
+        color: $theme_primary;
+        font-family: Segoe UI, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    input {
+        color: $theme_primary;
+    }
+
+    h2 {
+        color: $theme_primary;
+        font-size: 2.375rem;
+        font-weight: 400;
+    }
+</style>
