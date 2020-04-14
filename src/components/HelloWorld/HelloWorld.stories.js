2b67820bef45ffd83b53d38a831771d191b315b9
nickallama <me@nicholasneale.com>
2020-04-14 21:32:42 +0100

    Delete hello world component

--- src/components/HelloWorld/HelloWorld.stories.js
+++
@@ -1,8 +0,0 @@
-import HelloWorld from './HelloWorld';
-
-export default { title: 'HelloWorld' };
-
-export const asAComponent = () => ({
-    components: { HelloWorld },
-    template: '<HelloWorld msg="Welcome to Your Vue.js App" />'
-});
